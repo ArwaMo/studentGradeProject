e309cddcf60d5f0aea5b029184e4fce081361f88
ArwaMo <arwa-xx@hotmail.com>
2024-08-01 19:56:52 +0300

    Initial Commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+let grade = 88
+
+if(grade >= 90){
+    print("Excellent")
+}else if (grade >= 80){
+    print("Very Good")
+}else if(grade >= 70){
+    print("Good")
+}else if(grade >= 60){
+    print("Pass")
+}else if(grade < 60){
+    print("Fail")
+}
+
+
+
