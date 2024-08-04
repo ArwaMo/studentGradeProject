cbb1c1927174acc128db76fbd0a2ae3fe29c836b
ArwaMo <arwa-xx@hotmail.com>
2024-08-04 20:00:28 +0300

    convert if statement into switch case

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,18 +1,32 @@
-let grade = 88
-
-if(grade >= 90){
-    print("Excellent")
-}else if (grade >= 80){
-    print("Very Good")
-}else if(grade >= 70){
-    print("Good")
-}else if(grade >= 60){
-    print("Pass")
-}else if(grade < 60){
-    print("Fail")
-}
+let grade = 77
+//
+//if(grade >= 90){
+//    print("Excellent")
+//}else if (grade >= 80){
+//    print("Very Good")
+//}else if(grade >= 70){
+//    print("Good")
+//}else if(grade >= 60){
+//    print("Pass")
+//}else if(grade < 60){
+//    print("Fail")
+//}
 
 //this swift code 
 
 
 
+
+
+switch grade{
+case 90...100:
+    print("Excellent")
+case 80...90:
+    print("Very Good")
+case 70...80:
+    print("Good")
+case 60...70:
+    print("Pass")
+default:
+    print("Fail")
+}
